3e2430827ee8e54ee868b23e77924adfc59bdbb5
sostema <sostema@gmail.com>
2020-10-17 23:41:55 +0300

    quickfix

--- api/utils.py
+++ api/utils.py
@@ -1,15 +1,13 @@
-from typing import Dict, TypeVar, Generic
+from typing import Dict, Any
 
 import numpy as np
 from sklearn.neighbors import BallTree, DistanceMetric
 
 from database.entities.geographical_point import GeographicalPoint
 
-KV = TypeVar("KV")
 
-
-def get_radius_objects(center: GeographicalPoint, radius: float, objects: Dict[Generic[KV], GeographicalPoint]) \
-        -> Dict[Generic[KV], GeographicalPoint]:
+def get_radius_objects(center: GeographicalPoint, radius: float, objects: Dict[Any, GeographicalPoint]) \
+        -> Dict[Any, GeographicalPoint]:
     object_points = np.array([[point.latitude, point.longitude] for point in list(objects.values())])
     tree = BallTree(object_points, leaf_size=2, metric=DistanceMetric.get_metric("haversine"))
     object_idxs = tree.query_radius([center.latitude, center.longitude], r=radius)
